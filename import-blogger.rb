029836caba256f3dd4d3bdf5dc08627adeef707e
shantonu sarker <sarker.shantonu@gmail.com>
2021-01-31 05:17:12 +0600

    revised to old

--- import-blogger.rb
+++
@@ -1,7 +0,0 @@
-require "jekyll-import";
-
-    JekyllImport::Importers::Blogger.run({
-      "source"                => "blog-01-17-2021.xml",
-      "no-blogger-info"       => false,
-      "replace-internal-link" => false,
-    })
